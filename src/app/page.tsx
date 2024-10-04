379968dd134c4e5819591121b08c0d0bcd2806d7
Gianfranco Pisani <gianfranco.pisani@uc.cl>
2024-10-04 14:42:03 -0300

    fix: titulo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function LandingPage() {
         />
         <div className="absolute inset-0 flex flex-col items-center justify-center p-2">
           <Image src={logoDematOriginal} alt="Grupo DEMAT" width={400} height={400} className="mb-4" />
-          <h1 className="text-4xl md:text-6xl font-black text-center text-white">Revolucionando la Ferretería</h1>
+          <h1 className="text-4xl md:text-6xl font-black text-center text-white">Transformando la Ferretería</h1>
           <p className="mt-4 text-2xl md:text-4xl text-center text-white">Central de Compras para Ferreterías</p>
           <p className="mt-4 text-xl md:text-2xl text-center text-white">Únete al poder de compra para ser competitivo en el mercado</p>
           <Link href="/propuesta" className="mt-8 inline-block bg-secondary text-white hover:text-black transition-colors duration-500 py-3 px-6 rounded-md font-bold hover:bg-white text-center">
